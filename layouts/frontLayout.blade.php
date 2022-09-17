8c5a9bfbc3069b51c8bdbb5df5b0244957b13966
Davit Labadze <46887782+davitlabadze@users.noreply.github.com>
2022-09-17 23:48:10 +0400

    layout

---
+++ layouts/frontLayout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @vite('resources/css/app.css')
+
+    <title>Library</title>
+</head>
+
+<body class="bg-orange-100 select-none">
+    @yield('content')
+</body>
+
+</html>
